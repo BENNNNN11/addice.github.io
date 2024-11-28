f4f88f4511c6dc799a568603489b0ca3268ebf0d
wangjiabin01 <wangjiabin01@inspur.com>
2024-11-28 19:04:39 +0800

    20241128

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/addice.github.io/',
   bundler: viteBundler(),
   theme: defaultTheme({
     navbar: [
