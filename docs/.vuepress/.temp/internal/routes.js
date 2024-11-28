a16ec1605fbbfbbd6bdf3c78f25e1babbd6c0a0a
wangjiabin01 <wangjiabin01@inspur.com>
2024-11-28 19:10:15 +0800

    20241128

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -5,20 +5,3 @@ export const routes = Object.fromEntries([
   ["/Algorithm/Divide_Conquer.html", { loader: () => import(/* webpackChunkName: "Algorithm_Divide_Conquer.html" */"D:/datasets/addice-blogs/docs/.vuepress/.temp/pages/Algorithm/Divide_Conquer.html.js"), meta: {"title":"分治算法"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/datasets/addice-blogs/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
